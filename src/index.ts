6ee5c189c448d2d8c56e5b3aaec5df9ca9138bf5
Abner Persio <binhopersio@gmail.com>
2022-11-17 23:45:35 -0300

    chore: serve uploaded static files

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,19 @@
 import express from 'express';
+import path from 'node:path';
 
 import './config/env';
 import { connect } from './config/mongo';
+import { router } from './router';
 
 (async () => {
   const isConnected = await connect();
   if (!isConnected) return;
 
   const server = express();
+
+  server.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+  server.use(express.json());
+  server.use(router);
 
   server.listen(3001, () => console.log('Server is running'));
 })();
